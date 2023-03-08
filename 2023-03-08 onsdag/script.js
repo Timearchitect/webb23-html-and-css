c03ead8e25187312acabdb69ddbe5f0b20abdbb9
Alrik He <alrik.he@gritacademy.se>
2023-03-08 14:05:15 +0100

    responsive

---
+++ 2023-03-08 onsdag/script.js
@@ -0,0 +1,10 @@
+let cartLista = [];
+
+let addToCartObject = {
+    namn: "adidas", //firebase data[i].description
+    amount: 2, // document.getbyid(). input.value
+};
+
+cartLista.push(addToCartObject);
+
+console.log(cartLista);
